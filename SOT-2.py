dd04df6f8727417f57a3addd29e4ad2ebc9e3e78
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-01-22 19:13:34 +0100

    Task 2. [1] <abc> {~}
    Pobraną z klawiatury liczbę całkowitą zweryfikuj pod kątem parzystości. Wyświetl
    adekwatny komunikat, gdy jest lub nie jest parzysta

---
+++ SOT-2.py
@@ -0,0 +1,9 @@
+while True:
+    if int(input("Enter number integer divisible of 2: "))%2 == 0:
+        print("Number is OK")
+        break
+    else:
+        print("Number should be divisible of 2")
+
+
+    
