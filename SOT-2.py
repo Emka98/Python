c8772c52e8915ba56d85efdf0f75b1be6837fbfd
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-05-26 17:27:58 +0200

    Wyczyszczenie gałezi main

--- SOT-2.py
+++
@@ -1,9 +0,0 @@
-while True:
-    if int(input("Enter number integer divisible of 2: "))%2 == 0:
-        print("Number is OK")
-        break
-    else:
-        print("Number should be divisible of 2")
-
-
-    
