d1c4cc5435487e08bf10652fa85fb3ac0c749104
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-06-12 18:37:09 +0200

    Add files via upload

---
+++ Dice_Simulator.py
@@ -0,0 +1,41 @@
+import random
+
+
+def roll_dice(amount: int = 1) -> list[int]:
+    '''
+    Rolls some dice and returns the results as list.
+
+    :param amount: The amount od dice to roll.
+    :return: A list of doce rolls.
+    '''
+
+    if amount <= 0:
+        raise ValueError
+
+    rolls: list[int] = []
+    for i in range(amount):
+        random_roll: int = random.randint(1, 6)
+        rolls.append(random_roll)
+
+    return rolls
+
+
+def main():
+    while True:
+        try:
+            user_input: str = input("How many dice would you like to roll? ")
+
+            # To exit the game
+            if user_input.lower() == 'exit':
+                print('Thanks for playing!')
+                break
+
+            # Try to parse the user_input to int
+            print(*roll_dice(int(user_input)), sep=', ')
+
+        except ValueError:
+            print('(Please enter a valid number)')
+
+
+if __name__ == '__main__':
+    main()
