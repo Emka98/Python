30bea03e08123cc6fc10956e8df9c46eff354c5d
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-08-16 12:17:43 +0200

    Add files via upload

---
+++ 4/4.2.py
@@ -0,0 +1,19 @@
+import re
+
+char = (input("Eneter one letter or intiger: "))
+volvesEng = "A, E, I, O, U"
+consonants = "B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z"
+
+try:
+    int(char)
+    print(f"You entered number {char}")
+except:
+    if len(list(char)) == 1:
+        if re.search(volvesEng,char.capitalize()):
+            print("Entered letter is volve")
+        elif re.search(consonants,char.capitalize()):
+            print("Entered letter is consonants")       
+        else:
+            print("Entered char isn`t letter or intiger")
+    else:
+        print("Entered more than one char")
